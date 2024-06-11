825ac2a256c9da38391ddf37ba0d1aa17508b210
VictMan <vicbolgui@alu.edu.gva.es>
2024-06-11 22:20:29 +0200

    añadido política de privacidad en el pie

---
+++ src/Views/politicaPrivacidad.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../../public/css/styles.css">
+    <title>CashClubControl política de privacidad</title>
+</head>
+
+<body>
+    <div id="container">
+        <?php
+        include_once ('../includes/cabecera.php');
+        ?>
+        <main>
+            <div class="politica">
+                <h3>Política de privacidad</h3>
+                <p>En CashClubControl, la privacidad de nuestros usuarios es una prioridad para nosotros. Esta Política de
+                    Privacidad describe cómo recopilamos, utilizamos, divulgamos y protegemos la información personal de
+                    nuestros
+                    usuarios. Al utilizar nuestro sitio web, usted acepta los términos de esta Política de Privacidad.
+                </p>
+
+                <p>Recopilación de Información:<br>
+                    Recopilamos información personal de nuestros usuarios de diversas formas, como cuando se registran
+                    en
+                    nuestro
+                    sitio web, realizan una compra, se suscriben a nuestro boletín informativo, envían un formulario de
+                    contacto
+                    o
+                    interactúan con nuestras redes sociales. La información recopilada puede incluir su nombre,
+                    dirección de
+                    correo
+                    electrónico, dirección postal, número de teléfono, información demográfica y otra información
+                    relevante.</p>
+
+                <p>Uso de la Información:<br>
+                    Utilizamos la información recopilada para proporcionar y mejorar nuestros servicios, personalizar la
+                    experiencia
+                    del usuario, enviar comunicaciones relacionadas con el sitio web, procesar transacciones, responder
+                    a
+                    consultas
+                    y brindar soporte al cliente.</p>
+
+                <p>Divulgación de la Información:<br>
+                    No vendemos, intercambiamos ni transferimos de ninguna manera su información personal a terceros sin
+                    su
+                    consentimiento, excepto en los casos en que sea necesario para cumplir con la ley, hacer cumplir
+                    nuestras
+                    políticas o proteger nuestros derechos, propiedad o seguridad.</p>
+
+                <p>Cookies:<br>
+                    Utilizamos cookies y tecnologías similares para recopilar información sobre el uso de nuestro sitio
+                    web y
+                    mejorar la experiencia del usuario. Las cookies son pequeños archivos de texto que se almacenan en
+                    su
+                    dispositivo cuando visita nuestro sitio web. Puede optar por desactivar las cookies en la
+                    configuración de
+                    su
+                    navegador, pero esto puede afectar su experiencia de navegación en nuestro sitio web.</p>
+
+                <p>Seguridad:<br>
+                    Implementamos medidas de seguridad para proteger la información personal de nuestros usuarios. Sin
+                    embargo,
+                    ningún método de transmisión por Internet o almacenamiento electrónico es completamente seguro, por
+                    lo que
+                    no
+                    podemos garantizar la seguridad absoluta de la información.</p>
+
+                <p>Enlaces a Terceros:<br>
+                    Nuestro sitio web puede contener enlaces a sitios web de terceros. No somos responsables de las
+                    prácticas de
+                    privacidad o el contenido de estos sitios web. Le recomendamos que revise las políticas de
+                    privacidad de
+                    terceros antes de proporcionarles su información personal.</p>
+
+                <p>Cambios a la Política de Privacidad:<br>
+                    Nos reservamos el derecho de modificar esta Política de Privacidad en cualquier momento. Se
+                    publicarán las
+                    actualizaciones en esta página y se notificarán los cambios significativos a través de un aviso en
+                    nuestro
+                    sitio
+                    web. Le recomendamos que revise periódicamente esta Política de Privacidad para estar informado
+                    sobre cómo
+                    protegemos su información personal.</p>
+
+                <p>Si tiene alguna pregunta o inquietud sobre nuestra Política de Privacidad, no dude en ponerse en
+                    contacto con
+                    nosotros a través de la información de contacto proporcionada en nuestro sitio web.</p>
+            </div>
+        </main>
+    </div>
+</body>
+
+</html>
