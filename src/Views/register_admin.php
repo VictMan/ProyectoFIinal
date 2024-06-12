f01cbcc849dd228e43d1a98c87e151a03bc17587
VictMan <vicbolgui@alu.edu.gva.es>
2024-06-12 23:56:25 +0200

    añadido estilos a los distintos formularios de registro

--- src/Views/register_admin.php
+++ src/Views/register_admin.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear club</title>
+    <link rel="stylesheet" href="../../public/css/styles.css">
     <script src="../instruments/jquery-3.7.1.min.js"></script>
     <script src="../instruments/funciones.js"></script>
-    <link rel="stylesheet" href="../../public/css/styles.css">
-    <title>Crear club</title>
     <?php
     include_once ('../../Database/conexion.php');
-    include_once('../instruments/funcionesPHP.php');
+    include_once ('../instruments/funcionesPHP.php');
 
     $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
     $userName = isset($_POST['userName']) ? $_POST['userName'] : '';
@@ -41,10 +41,10 @@
             $conexion = conectarBD();
             $sql_match = "SELECT Nombre FROM Club WHERE Nombre = '$clubName'";
             $clubNameMatch = $conexion->query($sql_match);
-            
+
             if ($clubNameMatch->num_rows > 0) {
                 $clubNameErr = "Este nombre ya se está usando";
-            } else{
+            } else {
                 $clubNameErr = "";
             }
             desconectarBD($conexion);
@@ -100,6 +100,7 @@
 </head>
 
 <body>
+
     <script>
         $(document).ready(function () {
             $('#crearClub').click(function (e) {
@@ -108,51 +109,62 @@
                 var errorContainer = $('#error-container');
 
                 e.preventDefault();
-                
+
                 if (validatePassword(contraseña, contraseña2, errorContainer)) $('#nuevoClub').submit();
             });
         });
     </script>
-    <form id="nuevoClub" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
-        <label>Nombre y apellido:</label><br>
-        <input type="text" id="nombre" name="nombre" placeholder="Nombre y apellido del propietario" value="<?php if (isset($_POST['nombre'])) echo $_POST['nombre']; ?>">
-        <br>
-        <span class="error"><?php echo $nombreErr; ?></span>
-        <br><br>
+    <?php
+    include_once ('../includes/cabecera.php');
+    ?>
+    <main>
+        <form id="nuevoClub" class="registro" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+            <label>Nombre y apellido:</label><br>
+            <input type="text" id="nombre" name="nombre" placeholder="Nombre y apellido del propietario" value="<?php if (isset($_POST['nombre']))
+                echo $_POST['nombre']; ?>">
+            <br>
+            <span class="error"><?php echo $nombreErr; ?></span>
+            <br><br>
 
-        <label>Nombre de tu club o gimnasio:</label><br>
-        <input type="text" id="club" name="club" placeholder="Nombre de tu club o gimnasio" value="<?php if (isset($_POST['club'])) echo $_POST['club']; ?>">
-        <br>
-        <span class="error"><?php echo $clubNameErr; ?></span>
-        <br><br>
+            <label>Nombre de tu club o gimnasio:</label><br>
+            <input type="text" id="club" name="club" placeholder="Nombre de tu club o gimnasio" value="<?php if (isset($_POST['club']))
+                echo $_POST['club']; ?>">
+            <br>
+            <span class="error"><?php echo $clubNameErr; ?></span>
+            <br><br>
 
-        <label>Nombre de Usuario:</label>
-        <input type="text" id="userName" name="userName" placeholder="Nombre de Usuario" value="<?php echo $userName ?>">
-        <br>
-        <span class="error"><?php echo $userNameErr; ?></span>
-        <br><br>
+            <label>Nombre de Usuario:</label>
+            <input type="text" id="userName" name="userName" placeholder="Nombre de Usuario"
+                value="<?php echo $userName ?>">
+            <br>
+            <span class="error"><?php echo $userNameErr; ?></span>
+            <br><br>
 
-        <label>Correo electrónico:</label>
-        <input type="email" id="correo" name="correo" placeholder="Email" value="<?php echo $email ?>">
-        <br>
-        <span class="error"><?php echo $emailErr; ?></span>
-        <br><br>
+            <label>Correo electrónico:</label>
+            <input type="email" id="correo" name="correo" placeholder="Email" value="<?php echo $email ?>">
+            <br>
+            <span class="error"><?php echo $emailErr; ?></span>
+            <br><br>
 
-        <label>Contraseña:</label><br>
-        <input type="password" id="contraseña" name="contraseña" placeholder="Contraseña">
-        <br><br>
+            <label>Contraseña:</label><br>
+            <input type="password" id="contraseña" name="contraseña" placeholder="Contraseña">
+            <br><br>
 
-        <label>Confirma contraseña:</label><br>
-        <input type="password" id="contraseña2" placeholder="Confirma contraseña">
-        <span id='error-container' class='error'></span>
-        <br><br>
+            <label>Confirma contraseña:</label><br>
+            <input type="password" id="contraseña2" placeholder="Confirma contraseña">
+            <span id='error-container' class='error'></span>
+            <br><br>
 
-        <label>Subir Logo:</label><br>
-        <input type="file" id="logo" name="logo">
-        <br><br>
+            <label>Subir Logo:</label><br>
+            <input type="file" id="logo" name="logo">
+            <br><br>
 
-        <input type="submit" name="crearClub" id="crearClub" value="Crear">
-    </form>
+            <input type="submit" name="crearClub" id="crearClub" value="Crear">
+        </form>
+    </main>
+    <?php
+    include_once ('../includes/pie.html');
+    ?>
 </body>
 
 </html>
