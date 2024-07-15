3fb363da470700727909376753602df38bfff7ef
VictMan <vicbolgui@alu.edu.gva.es>
2024-07-15 19:15:25 +0200

    SI

--- src/Views/restablecer_password.php
+++ src/Views/restablecer_password.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="../instruments/jquery-3.7.1.min.js"></script>
     <script src="../instruments/funciones.js"></script>
+    <link rel="stylesheet" href="../../public/css/styles.css">
     <title>Restablecer mi contraseña</title>
 </head>
 
